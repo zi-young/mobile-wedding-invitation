9dbe364ca68df1a6d8606ae2f73efaa748190caa
jypark <jypark912@monkey-soft.com>
2025-08-19 08:24:14 +0900

    오류해결

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import WeddingInvitationContent from "./WeddingInvitationContent"
 
-export default function HomePage() {
-  return <WeddingInvitationContent currentPageIdentifier="default" />
+export default function RootPage() {
+  return <WeddingInvitationContent currentPageIdentifier="1" />
 }
