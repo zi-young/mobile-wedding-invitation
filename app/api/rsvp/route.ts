386d8cc7eef3ad5a3e11a5e02e50ac0629facb1e
jypark <jypark912@monkey-soft.com>
2025-08-26 08:47:01 +0900

    구글시트주소변경

--- app/api/rsvp/route.ts
+++ app/api/rsvp/route.ts
@@ -4,7 +4,7 @@ export async function POST(req: Request) {
   try {
     const data = await req.json();
 
-    const res = await fetch("https://script.google.com/macros/s/AKfycbzcLtJMkA0o20-4SrM-8Pyz7aGv85fxBAcatNe7S4oiYr8V-B-RnIoz3ehY13kFp4Ez0Q/exec", {
+    const res = await fetch("https://script.google.com/macros/s/AKfycbwdNsLdye4YbSxaKQPeA6rnvNndoiEP79hpe5ogE7UraS1rYn8ekVEH1nQdfhHyFMLDfQ/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
