7fce636c5cfe8ebf52f088d12e3e12309e7a3a9c
jypark <jypark912@monkey-soft.com>
2025-08-27 14:44:07 +0900

    수정

--- app/[pageId]/page.tsx
+++ app/[pageId]/page.tsx
@@ -1,5 +1,13 @@
+import { notFound } from "next/navigation";
 import WeddingInvitationContent from "../WeddingInvitationContent"
 
 export default function Page({ params }: { params: { pageId: string } }) {
+
+  const { pageId } = params;
+  // /admin 접근은 여기서 404 처리
+  if (pageId === "admin") {
+    return notFound(); // /admin은 이 페이지에서 렌더링 안됨
+  }
+
   return <WeddingInvitationContent currentPageIdentifier={params.pageId} />
 }
